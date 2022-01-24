cec24837b5f352a87240971c4b3b9bcba41f2940
Dr. Farheen Fatima <farheenfatima@Syeds-MacBook-Air.local>
2022-01-25 03:54:05 +0530

    Initial Comment

---
+++ script.js
@@ -0,0 +1,11 @@
+var count = 0;
+
+function incre() {
+  count++;
+  document.getElementById('tnum').innerHTML = count;
+}
+
+function reset() {
+  count = 0;
+  document.getElementById('tnum').innerHTML = count;
+}
